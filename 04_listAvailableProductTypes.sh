7b7e6dd584d354930aa06238b97163f1a6012b95
fromcloud <outspace99@gmail.com>
2015-04-02 14:47:42 +0900

    2nd commit

--- 04_listAvailableProductTypes.sh
+++ 04_listAvailableProductTypes.sh
@@ -3,4 +3,5 @@
 cloudmonkey set profile server 1>/dev/null
 cloudmonkey listAvailableProductTypes \
 			zoneid=eceb5d65-6571-4696-875f-5a17949f3317 \
-			filter=productid,productstate,serviceofferingid,templateid,diskofferingid,diskofferingdesc,zoneid,zonedesc | grep -E 'cent.*6.4.*64bit.*1x1' -7
+			filter=productid,productstate,serviceofferingid,templateid,diskofferingid,diskofferingdesc,zoneid,zonedesc \
+			| grep -E 'cent.*6.4.*64bit.*1x1' -7
