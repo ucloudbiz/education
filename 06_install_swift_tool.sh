3c4a1c9e1fda27635d28a9b9bffbeaf66c2a74f9
fromcloud <outspace99@gmail.com>
2015-04-08 16:49:27 +0900

    9th commit

--- 06_install_swift_tool.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yum install python-setuptools
-easy_install pip
-pip install --upgrade setuptools
-pip install python-swiftclient
-pip install certifi
-yum install libffi libffi-devel gcc glibc python-devel openssl-devel
-pip install pyopenssl ndg-httpsclient pyasn1
